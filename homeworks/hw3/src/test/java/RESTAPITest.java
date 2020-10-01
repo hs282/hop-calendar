9c6a435c241e599b94a95cca4dfbfc3bcf5b4cb3
jangwoopark <pjwoo3@gmail.com>
2020-10-01 21:20:12 +0900

    added testBooks, testAddBook, and testDelAuthor

--- homeworks/hw3/src/test/java/RESTAPITest.java
+++ homeworks/hw3/src/test/java/RESTAPITest.java
@@ -38,4 +38,46 @@ public class RESTAPITest {
     }
 
     // TODO: Add your tests here
+
+    @Test
+    public void testBooks() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://localhost:7000/books")
+                .build();
+        Response response = client.newCall(request).execute();
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    public void testAddBook() throws IOException {
+        // add Moby Dick to Books
+        RequestBody postBody = new FormBody.Builder()
+                .add("title", "Moby Dick")
+                .add("isbn", "9780141199603")
+                .add("publisher", "Penguin")
+                .add("year", "2013")
+                // what TODO for authorId?
+                .add("authorId", "")
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:7000/addbook")
+                .post(postBody)
+                .build();
+        Response response = client.newCall(request).execute();
+        assertEquals(201, response.code());
+    }
+
+    @Test
+    public void testDelAuthor() throws IOException {
+        RequestBody postBody = new FormBody.Builder()
+                .add("name", "Sadegh Hedayat")
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:7000/delauthor")
+                .post(postBody)
+                .build();
+        Response response = client.newCall(request).execute();
+        assertEquals(200, response.code());
+    }
+
 }
