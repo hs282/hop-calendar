af81ae8dc30ae59b1b18fb188c92bae2fa48b827
tchung17 <60721329+tchung17@users.noreply.github.com>
2020-10-06 14:37:20 -0400

    commit

--- project/backend/.idea/testbackend/course.js
+++ project/backend/.idea/testbackend/course.js
@@ -3,15 +3,13 @@ class Course {
 	constructor(course) {
         //optional constructor parameter
 		if (!course) {
-            this.tasks = []
             this.name = ""
-			this.numTasks = 0
-			this.courseId = 0
+            this.courseId = 0
+            this.tasks = []
 		} else {
-            this.tasks = course.tasks
             this.name = course.name
-			this.numTasks = course.numTasks
-			this.courseId = course.courseId
+            this.courseId = course.courseId
+            this.tasks = course.tasks
 		}
     }
     get name() {
---
+++ project/backend/.idea/testbackend/index.js
@@ -0,0 +1,32 @@
+"use strict";
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const app = express();
+app.use(cors());
+app.use(bodyparser.json());
+
+//import our Classes
+var Course = require('./course.js');
+var Task = require('./task.js');
+var Student = require('./student.js');
+var Instructor = require('./instructor.js');
+
+//dummy data
+let homework = new Task({ type: 'assignment', deadline: "11:59PM", taskId: 0, blurb: "Please submit before midnight"});
+let test = new Task({ type: 'examination', deadline: "1:30PM", taskId: 1, blurb: "Please turn on your webcams"});
+
+let algebra = new Course({ name: 'algebra', tasks: [0, 1], courseId: 3})
+
+//stringify to store in database
+let asString = JSON.stringify(algebra);
+
+//back to object to send to frontend/use class methods
+let backToObj = JSON.parse(asString);
+
+//test endpoint
+app.get('/', (req, res) => {
+    res.send(backToObj)
+})
+
+app.listen(3000);
