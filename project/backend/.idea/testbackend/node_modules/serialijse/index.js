44b15c93283d756778ecad7e89a881147107ff4a
tchung17 <60721329+tchung17@users.noreply.github.com>
2020-10-06 14:40:48 -0400

    commit

--- project/backend/.idea/testbackend/node_modules/serialijse/index.js
+++
@@ -1,7 +0,0 @@
-/*global exports,require*/
-var lib = require("./lib/serialijse");
-exports.serialize = lib.serialize;
-exports.deserialize = lib.deserialize;
-exports.serializeZ = lib.serializeZ;
-exports.deserializeZ = lib.deserializeZ;
-exports.declarePersistable = lib.declarePersistable;
