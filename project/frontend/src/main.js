a7fd135c1029f7ee9a02dc3a4fc5b3bb76907283
tchung17 <60721329+tchung17@users.noreply.github.com>
2020-10-15 13:58:00 -0400

    frontend

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/en";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false
-Vue.use(Vuetify)
+//Vue.use(Vuetify)
+Vue.use(ElementUI, { locale });
+
 new Vue({
   router,
   store,
-  Vuetify,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
