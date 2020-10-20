ebd92e0385c0b7e6c49d9f76239e89c08b619594
Hyunsu Shin <happyfeller29@ymail.com>
2020-10-20 18:48:57 -0400

    hard coded the courses displayed

--- project/frontend/src/views/DropCourses.vue
+++ project/frontend/src/views/DropCourses.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <h1 style="padding-left: 50px">All Courses</h1><br>
+        <h1 style="padding-left: 50px">Drop Courses</h1><br>
+        <!--<li v-for="course in courses">
+            {{ course.name }}
+        </li>-->
         <form style="padding-left: 100px">
             <input type="checkbox"> Data Structures<br><br>
             <input type="checkbox"> Computer Ethics<br><br>
@@ -9,8 +12,8 @@
         </form>
         
         <p style="padding-left: 300px">
-            <el-button style="background-color:#008CBA; color:white" @click="add">
-                Add
+            <el-button style="background-color:#008CBA; color:white" @click="drop">
+                Drop
                 </el-button>
         </p>
     </div>
@@ -18,7 +21,7 @@
 
 <script>
     export default {
-        
+      
     }
 </script>
 
--- project/frontend/src/views/InstructorCourses.vue
+++ project/frontend/src/views/InstructorCourses.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
+        <h1 style="padding-left: 50px">My Courses</h1><br>
+        <ul style="padding-left:100px">
+            <h2>Object Oriented Software Engineering</h2><br>
+            <input type="checkbox"> 10/25: Midterm 1<br><br>
+            <input type="checkbox"> 10/30: HW3 due<br><br>
+            <el-button style="background-color:#008CBA; color:white" @click="addtask">
+                Add task
+                </el-button>
+            <el-button style="background-color:#008CBA; color:white" @click="deletetask">
+                Delete task
+                </el-button>
+            <el-button style="background-color:#008CBA; color:white" @click="edittask">
+                Edit task
+                </el-button>
+        </ul>
 
     </div>
 </template>
