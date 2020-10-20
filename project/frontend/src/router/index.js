27ff9d10411ff5510d432c91126e0c2f23af5ad0
Hyunsu Shin <happyfeller29@ymail.com>
2020-10-20 18:50:30 -0400

    index.js

--- project/frontend/src/router/index.js
+++ project/frontend/src/router/index.js
@@ -2,25 +2,43 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import DropCourses from '../views/DropCourses.vue'
+import AllCourses from '../views/AllCourses.vue'
+import InstructorCourses from '../views/InstructorCourses.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/dropcourses',
+        name: 'DropCourses',
+        component: DropCourses
+    },
+    {
+        path: '/allcourses',
+        name: 'AllCourses',
+        component: AllCourses
+    },
+    {
+        path: '/instructorcourses',
+        name: 'InstructorCourses',
+        component: InstructorCourses
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
