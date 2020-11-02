b2de62d61a6804f22e79813421d23fd46cc35e33
inpyo-git <70837899+inpyo-git@users.noreply.github.com>
2020-11-03 03:13:14 +0900

    failed to update to postgre

--- project/backend/db.js
+++ project/backend/db.js
@@ -1,11 +1,39 @@
-
 import Sequelize from 'sequelize'
 import Student from './database-models/Student.js';
 import Instructor from './database-models/Instructor.js';
 import Course from './database-models/Course.js';
 import Task from './database-models/Task.js';
+
+
+//create database prior to runnign
+//change this according to your local database
+const user = 'postgres'
+const host = 'localhost'
+const password = 'postgres'
+const port = '3306'
+const database = 'temp'
+
 // Option 1: Passing a connection URI
-const sequelize = new Sequelize('mysql://root:fantasticsniffle@localhost:3306/sys');
+
+const sequelize = new Sequelize(database, user, password, {
+  host,
+  port,
+  dialect: 'postgres',
+  logging: false
+});
+
+// const models = {
+//   students: sequelize.import('./database-models/Student.js')
+//   instructors: sequelize.import('./database-models/Instructor.js')
+//   courses: sequelize.import('./database-models/Course.js')
+//   tasks: sequelize.import('./database-models/Task.js')
+// };
+// Object.keys(models).forEach(modelName) => {
+//   if('associate' in models[modelName]) {
+//     models[modelName].associate(models);
+//   }
+// });
+//const sequelize = new Sequelize('postgres://postgres:971106@localhost:7000');
 
 (async function main() {
     try {
@@ -22,3 +50,6 @@ const sequelize = new Sequelize('mysql://root:fantasticsniffle@localhost:3306/sy
       }
 })();
 
+// models.sequelize = sequelize;
+// models.Sequelize = Sequelize;
+// export default models;
