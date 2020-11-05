60fad2a829e9af78164fbe57bc0f9aa413259c63
Hyunsu Shin <happyfeller29@ymail.com>
2020-11-04 19:25:37 -0500

    login not working

--- project/frontend/src/views/Login.vue
+++ project/frontend/src/views/Login.vue
@@ -1,20 +1,21 @@
+
 <template>
     <div style="height : 100vh; display:flex; justify-content:center; align-items:center;">
         <el-card style="height: 500px; width: 500px; display:flex; justify-content:center; align-items:center;">
             <h1>Hop Calendar</h1>
             <el-form>
-                <el-form-item>
-                    <el-radio v-model="role" label="student">Student</el-radio>
-                    <el-radio v-model="role" label="instructor">Instructor</el-radio>
+                <el-form-item action='/login'>
+                    <el-radio v-model="role" label="student" id="student">Student</el-radio>
+                    <el-radio v-model="role" label="instructor" id="instructor">Instructor</el-radio>
                 </el-form-item>
                 <el-form-item label="JHED ID">
-                    <el-input v-model="email"></el-input>
+                    <el-input v-model="email" id="input_email"></el-input>
                 </el-form-item>
                 <el-form-item label="HopCal Password">
-                    <el-input v-model="password"></el-input>
+                    <el-input v-model="password" id="input_pw"></el-input>
                 </el-form-item>
             </el-form>
-            <el-button @click="login">
+            <el-button id="login" @click="login()">
                 Login
             </el-button>
         </el-card>
@@ -32,7 +33,21 @@
         },
         methods: {
             login() {
-                
+                /*let user = document.getElementById("input_email");
+                let pw = document.getElementById("input_pw");
+                let role = "student";
+                if (document.getElementById("instructor").checked) {
+                    role = "instructor";
+                }
+                fetch(`http://localhost:8080/login?username=${username}&password=${password}&role=${role}`, {
+                    method: 'GET'
+                    }   
+                ).then(res => window.location.reload());*/
+                if (document.getElementById("instructor").checked) {
+                    this.$router.push('/instructorcourses')
+                } else {
+                    this.$router.push('/')
+                }
             }
         },
     }
