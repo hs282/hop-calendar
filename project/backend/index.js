ba04f8ed3aa3e8cbf4409b1d227902364941e962
jangwoopark <pjwoo3@gmail.com>
2020-11-17 17:30:54 +0900

    testing heroku

--- project/backend/index.js
+++ project/backend/index.js
@@ -36,6 +36,10 @@ import Task from './database-models/Task.js';
 //     }
 //   })
 
+app.get('/', async (req, res) => {
+    console.log("get / endpoint")
+})
+
 //endpoint login => find which student it is 
 app.post('/login', async (req, res) => {
     //get username, password and rolefrom req object role
