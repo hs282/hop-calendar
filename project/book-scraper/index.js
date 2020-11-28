3b60f1b33cd191cc9b4bdc740e26ebc9c4d292d0
inpyo-git <70837899+inpyo-git@users.noreply.github.com>
2020-11-29 02:23:18 +0900

    test

--- project/book-scraper/index.js
+++ project/book-scraper/index.js
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:01cc26abcf9f251830ba4c72ed3754bb8e5e8049310f32f8094315c641fd43ac
-size 299
+const browserObject = require('./browser');
+const scraperController = require('./pageController');
+
+//Start the browser and create a browser instance
+let browserInstance = browserObject.startBrowser();
+
+// Pass the browser instance to the scraper controller
+scraperController(browserInstance)
+
+//dummy test
