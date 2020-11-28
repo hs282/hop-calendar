959365fd68e62c74fc759aa7f6a2f4c5cd65916e
inpyo-git <70837899+inpyo-git@users.noreply.github.com>
2020-11-29 04:43:56 +0900

    semi working version

--- project/book-scraper/pageScraper.js
+++ project/book-scraper/pageScraper.js
@@ -1,11 +1,24 @@
 //test
 const scraperObject = {
-    url: 'http://books.toscrape.com',
+    url: 'https://www.gradescope.com/auth/saml/jhu',
     async scraper(browser){
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url);
 
+        
+        // Wait for the required DOM to be rendered
+        await page.waitForSelector('.courseList--coursesForTerm');
+        // Get the link to all the required books
+        let urls = await page.$$eval('a', links => {
+            // Make sure the book to be scraped is in stock
+            //links = links.filter(link => link.innerText !== "Fall 2020")
+            // Extract the links from the data
+            links = links.map(el =>  el.href)
+            return links;
+        });
+
+        console.log(urls);
     }
 }
 
