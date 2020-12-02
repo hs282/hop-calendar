db0678884efb5df9631408f15c437687ea4af7ab
Jang Woo Park <jangwoopark@Jangui-MacBookAir.local>
2020-12-02 14:11:39 +0900

    send email update upon account validation

--- project/frontend/src/views/HopCalAdmin.vue
+++ project/frontend/src/views/HopCalAdmin.vue
@@ -65,6 +65,45 @@
                         courses: potentialI.courses, //potential instructor's courses
                     }
                 )
+                
+                // send email to instructor upon acceptance
+                var Email = { send: function (a) { 
+                        return new Promise(function (n, e) { 
+                            a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; 
+                            var t = JSON.stringify(a);
+                            Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { 
+                                n(e) 
+                                })
+                            }) 
+                        }, ajaxPost: function (e, n, t) { 
+                                var a = Email.createCORSRequest("POST", e); 
+                                a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { 
+                                    var e = a.responseText; 
+                                    null != t && t(e) 
+                                }, 
+                                a.send(n) 
+                        }, ajax: function (e, n) { 
+                                var t = Email.createCORSRequest("GET", e); 
+                                t.onload = function () { 
+                                    var e = t.responseText; 
+                                    null != n && n(e) 
+                                }, 
+                                t.send() 
+                        }, createCORSRequest: function (e, n) { 
+                                var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t 
+                        } 
+                    };
+
+                    Email.send({
+                        Host : "smtp.gmail.com",
+                        Username : "fantasticsniffle@gmail.com",
+                        Password : "fan12345!",
+                        To : this.email,
+                        From : "fantasticsniffle@gmail.com",
+                        Subject : "Thank you for joining HopCalendar!",
+                        Body : "Congratulations! Your HopCalendar instructor account has been successfully created."
+                    });
+
                 this.removePotentialInstructor(potentialI.username)
                 this.getPotentialInstructors()
             }
